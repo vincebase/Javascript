eb4c5abbd6c52683a7363bfb62f545e48eb376db
Vince Andrei Labastida <71559303+DeVinciCodeee@users.noreply.github.com>
2021-05-26 15:11:29 +0800

    Soundboard

---
+++ public/index.js
@@ -0,0 +1,19 @@
+const sounds = ["among us",
+"toast bread butter jam",
+"toast big iq", "up and down",
+"who's impostor"]
+
+
+sounds.forEach(sound => {
+    const btns = document.createElement('button')
+    btns.classList.add('btn', 'btn-dark', 'me-3')
+    
+    btns.innerHTML = sound;
+
+    btns.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+
+    document.getElementById('buttons').appendChild(btns)
+})
